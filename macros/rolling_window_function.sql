95e5b8cf14cf644817ee650cf2a612ef82618e84
eKatia <ekaterina.khrushch@contentful.com>
2023-08-05 17:49:36 +0000

    add new macro and config update

---
+++ macros/rolling_window_function.sql
@@ -0,0 +1,7 @@
+{% macro rolling_window_function(column_name, partition_by, order_by, aggregation_type='avg', rolling_window_number=7) %}
+    {{aggregation_type}}( {{ column_name }} ) OVER (
+                PARTITION BY {{ partition_by }}
+                ORDER BY {{ order_by }}
+                ROWS BETWEEN {{rolling_window_number}} PRECEDING AND CURRENT ROW
+            ) AS avg_{{rolling_window_number}}_periods_{{ column_name }}
+{% endmacro %}
